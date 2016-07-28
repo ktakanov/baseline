0a5a3c64afaa78f50b894411d8e7547d1c466c73
Pavel Danilov <xzergx94@gmail.com>
2016-07-28 03:06:12 +0300

    Unit tests for feature extraction

--- src/main/feature_extraction.py
+++ src/main/feature_extraction.py
@@ -3,64 +3,66 @@ import numpy as np
 
 
 def extract_what_to_buy(clicks_gb):
-    f3 = clicks_gb.apply(counts)
-    f6 = clicks_gb.apply(sequent_clicks)
-    f7 = clicks_gb.apply(max_duration_between_sequent_clicks)
-    f3, f6, f7 = map(lambda df: df.reset_index().rename(columns={'level_1': 'Item ID'}), [f3, f6, f7])
+    f3 = extract_f3(clicks_gb)
+    f6 = extract_f6(clicks_gb)
+    f7 = extract_f7(clicks_gb)
 
     return {'f3': f3, 'f6': f6, 'f7': f7}
 
 
-def get_df_features(grouped, func):
-    features_list = []
+def extract_f3(clicks_gb):
 
-    for session_id in grouped.groups.iterkeys():
-        session_grouped_by_items = grouped.get_group(session_id)
-        group_func = func(session_grouped_by_items).reset_index().rename(columns={'index': 'Item ID'})
-        group_func.insert(loc=0, column='Session ID', value=session_id)
-        features_list.append(group_func)
+    def counts(group):
+        return pd.Series(group['Item ID'].value_counts(), name='Counts')
 
-    return pd.concat(features_list, ignore_index=True)
+    return clicks_gb.apply(counts).reset_index().rename(columns={'level_1': 'Item ID'})
 
 
-def counts(group):
-    return pd.Series(group['Item ID'].value_counts(), name='Counts')
+def extract_f6(clicks_gb):
+
+    def sequent_clicks(group):
+        diff = group['Item ID'].diff()
+        diff_reverse = group['Item ID'].diff(periods=-1)
+        repeated = ((diff == 0) | (diff_reverse == 0)).apply(lambda x: np.int32(x))
+        group.insert(loc=group.shape[1], column='Sequent Clicks', value=repeated)
+
+        return group\
+            .groupby('Item ID')['Sequent Clicks']\
+            .sum()\
+            .apply(lambda x: 1 if x == 0 else x)
+
+    return clicks_gb.apply(sequent_clicks).reset_index().rename(columns={'level_1': 'Item ID'})
 
 
-def sequent_clicks(group):
-    diff = group['Item ID'].diff()
-    diff_reverse = group['Item ID'].diff(periods=-1)
-    repeated = ((diff == 0) | (diff_reverse == 0)).apply(lambda x: np.int32(x))
-    group.insert(loc=group.shape[1], column='Sequent Clicks', value=repeated)
+def extract_f7(clicks_gb):
 
-    return group\
-        .groupby('Item ID')['Sequent Clicks']\
-        .sum()\
-        .apply(lambda x: 1 if x == 0 else x)
+    def max_duration_between_sequent_clicks(group):
+        positive_key = np.int8(1)
+        negative_key = np.int8(0)
+        repeated = group['Item ID'].diff().apply(lambda x: positive_key if x == 0 else negative_key)
+        time_diff = group['Timestamp'].diff().astype('timedelta64[ms]')
+        group.insert(loc=group.shape[1], column='Time Difference', value=time_diff)
+        group.insert(loc=group.shape[1], column='Repeated Click', value=repeated)
+        group_by_repeated = group.groupby('Repeated Click')
 
+        result_pos =\
+            group_by_repeated.get_group(positive_key).groupby('Item ID')['Time Difference'].max()\
+            if positive_key in group_by_repeated.groups.keys()\
+            else\
+            None
 
-def max_duration_between_sequent_clicks(group):
-    positive_key = 1
-    negative_key = 0
-    repeated = group['Item ID'].diff().apply(lambda x: positive_key if x == 0 else negative_key)
-    time_diff = group['Timestamp'].diff().astype('timedelta64[ms]')
-    group.insert(loc=group.shape[1], column='Time Difference', value=time_diff)
-    group.insert(loc=group.shape[1], column='Repeated Click', value=repeated)
-    group_by_repeated = group.groupby('Repeated Click')
+        result_neg =\
+            group_by_repeated.get_group(negative_key).groupby('Item ID')['Time Difference']\
+                .apply(lambda x: np.float64(np.iinfo(np.int64).max))\
+            if negative_key in group_by_repeated.groups.keys()\
+            else\
+            None
 
-    result_pos =\
-        group_by_repeated.get_group(positive_key).groupby('Item ID')['Time Difference'].max()\
-        if positive_key in group_by_repeated.groups.keys()\
-        else\
-        None
+        result = pd.concat([result_pos, result_neg]).sort_values()
 
-    result_neg =\
-        group_by_repeated.get_group(negative_key).groupby('Item ID')['Time Difference'].apply(lambda x: np.float32(0))\
-        if negative_key in group_by_repeated.groups.keys()\
-        else\
-        None
+        return result[~result.index.duplicated()]
 
-    return pd.concat([result_pos, result_neg])
+    return clicks_gb.apply(max_duration_between_sequent_clicks).reset_index().rename(columns={'level_1': 'Item ID'})
 
 
 def extract_buy_or_not(clicks_gb, features_what_to_buy):
@@ -74,6 +76,42 @@ def extract_buy_or_not(clicks_gb, features_what_to_buy):
     p11 = features_what_to_buy['f7'].groupby('Session ID')['Time Difference'].max()
 
     return np.array([p1, p2, p3, p4, p5, p6, p10, p11], dtype=np.float32).transpose()
+
+
+def extract_p1(clicks_gb):
+    return clicks_gb.size()
+
+
+def extract_p2(f3):
+    return f3.groupby('Session ID')['Counts'].mean()
+
+
+def extract_p3(clicks_gb):
+    return clicks_gb.apply(lambda x: x['Timestamp'].iloc[-1] - x['Timestamp'].iloc[0]).astype('timedelta64[ms]')
+
+
+def extract_p4(clicks_gb):
+    return clicks_gb.apply(lambda x: x['Timestamp'].diff().astype('timedelta64[ms]').fillna(np.float64(0)).max())
+
+
+def extract_p5(p1, p3):
+    return p3.divide(p1 - 1).replace([np.inf, -np.inf], 0.0)
+
+
+def extract_p6(f3):
+    return f3.groupby('Session ID')['Counts'].max()
+
+
+def extract_p10(f6):
+    return f6.groupby('Session ID')['Sequent Clicks'].max()
+
+
+def extract_p11(f7):
+    int64_max = np.iinfo(np.int64).max
+
+    return f7.groupby('Session ID')['Time Difference']\
+        .apply(lambda group: group[group < int64_max].max())\
+        .fillna(int64_max)
 
 
 def extract_buys(clicks_group_keys, buys_group_keys):
---
+++ src/test/suite.py
@@ -0,0 +1,140 @@
+import unittest
+import pandas as pd
+import numpy as np
+from pandas.tslib import Timestamp
+from src.main.feature_extraction import extract_f3, extract_f6, extract_f7, extract_p1, extract_p2, extract_p3,\
+    extract_p4, extract_p5, extract_p6, extract_p10, extract_p11
+
+
+class TestFeatureExtraction(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+        self.df = pd.DataFrame(
+            {'Session ID': pd.Series([5, 5, 5, 10, 10, 15, 15, 15, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25, 30, 30],
+                                     dtype=np.int32),
+             'Timestamp': pd.Series([
+                 Timestamp('2014-04-07 17:13:46.713'),
+                 Timestamp('2014-04-07 17:20:56.973'),
+                 Timestamp('2014-04-07 17:21:19.602'),
+                 Timestamp('2014-04-04 07:44:14.590'),
+                 Timestamp('2014-04-04 07:45:20.245'),
+                 Timestamp('2014-04-05 08:14:28.645'),
+                 Timestamp('2014-04-05 08:15:49.200'),
+                 Timestamp('2014-04-05 08:19:55.455'),
+                 Timestamp('2014-04-03 14:42:25.879'),
+                 Timestamp('2014-04-03 14:42:43.585'),
+                 Timestamp('2014-04-03 14:44:40.147'),
+                 Timestamp('2014-04-03 14:45:09.199'),
+                 Timestamp('2014-04-07 03:32:19.078'),
+                 Timestamp('2014-04-07 03:32:44.139'),
+                 Timestamp('2014-04-07 03:32:47.525'),
+                 Timestamp('2014-04-07 03:32:48.647'),
+                 Timestamp('2014-04-07 03:32:57.321'),
+                 Timestamp('2014-04-07 03:32:59.910'),
+                 Timestamp('2014-04-06 11:11:19.232'),
+                 Timestamp('2014-04-06 11:11:19.232')]),
+             'Item ID': np.array([214530776, 214530776, 214530776, 214820942, 214826810, 214555903, 214547255,
+                                  214547255, 214829282, 214718203, 214829282, 214819552, 214836761, 214839313,
+                                  214839313, 214839313, 214839313, 214839313, 214820201, 214820201],
+                                 dtype=np.int32)
+             }
+        )
+        self.gb = self.df.groupby('Session ID')
+
+    def test_f3(self):
+        src = extract_f3(self.gb).sort_values(['Session ID', 'Item ID'])
+        dst = pd.DataFrame(np.array([
+            [5, 214530776, 3],
+            [10, 214820942, 1],
+            [10, 214826810, 1],
+            [15, 214555903, 1],
+            [15, 214547255, 2],
+            [20, 214829282, 2],
+            [20, 214718203, 1],
+            [20, 214819552, 1],
+            [25, 214836761, 1],
+            [25, 214839313, 5],
+            [30, 214820201, 2]],
+            dtype=np.int32),
+            columns=['Session ID', 'Item ID', 'Counts']
+        ).sort_values(['Session ID', 'Item ID'])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of F3')
+
+    def test_f6(self):
+        src = extract_f6(self.gb).sort_values(['Session ID', 'Item ID'])
+        dst = pd.DataFrame(np.array([
+            [5, 214530776, 3],
+            [10, 214820942, 1],
+            [10, 214826810, 1],
+            [15, 214555903, 1],
+            [15, 214547255, 2],
+            [20, 214829282, 1],
+            [20, 214718203, 1],
+            [20, 214819552, 1],
+            [25, 214836761, 1],
+            [25, 214839313, 5],
+            [30, 214820201, 2]],
+            dtype=np.int32),
+            columns=['Session ID', 'Item ID', 'Sequent Clicks']
+        ).sort_values(['Session ID', 'Item ID'])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of F6')
+
+    def test_f7(self):
+        int64_max = np.iinfo(np.int64).max
+        src = extract_f7(self.gb).sort_values(['Session ID', 'Item ID'])
+        dst = pd.DataFrame(np.array([
+            [5, 214530776, 430260],
+            [10, 214820942, int64_max],
+            [10, 214826810, int64_max],
+            [15, 214555903, int64_max],
+            [15, 214547255, 246255],
+            [20, 214829282, int64_max],
+            [20, 214718203, int64_max],
+            [20, 214819552, int64_max],
+            [25, 214836761, int64_max],
+            [25, 214839313, 8674],
+            [30, 214820201, 0]]),
+            columns=['Session ID', 'Item ID', 'Time Difference']
+        ).sort_values(['Session ID', 'Item ID'])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of F7')
+
+    def test_p1(self):
+        src = extract_p1(self.gb)
+        dst = np.array([3, 2, 3, 4, 6, 2])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of P1')
+
+    def test_p2(self):
+        src = extract_p2(extract_f3(self.gb))
+        dst = np.array([3, 1, 1.5, 4.0 / 3.0, 3, 2])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of P2')
+
+    def test_p3(self):
+        src = extract_p3(self.gb)
+        dst = np.array([452889.0, 65655.0, 326810.0, 163320.0, 40832.0, 0.0])
+        np.testing.assert_array_almost_equal(src, dst, decimal=-3, err_msg='Incorrect extraction of P3')
+
+    def test_p4(self):
+        src = extract_p4(self.gb)
+        dst = np.array([430260.0, 65655.0, 246255.0, 116562.0, 25061.0, 0.0])
+        np.testing.assert_array_almost_equal(src, dst, decimal=-3, err_msg='Incorrect extraction of P4')
+
+    def test_p5(self):
+        src = extract_p5(extract_p1(self.gb), extract_p3(self.gb))
+        dst = np.array([226444.5, 65655.0, 163405.0, 54440.0, 8166.3999999999996, 0.0])
+        np.testing.assert_array_almost_equal(src, dst, decimal=-3, err_msg='Incorrect extraction of P5')
+
+    def test_p6(self):
+        src = extract_p6(extract_f3(self.gb))
+        dst = np.array([3, 1, 2, 2, 5, 2])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of P6')
+
+    def test_p10(self):
+        src = extract_p10(extract_f6(self.gb))
+        dst = np.array([3, 1, 2, 1, 5, 2])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of P10')
+
+    def test_p11(self):
+        src = extract_p11(extract_f7(self.gb))
+        dst = np.array([430260.0, 9.2233720368547758e+18, 246255.0, 9.2233720368547758e+18, 8674.0, 0.0])
+        np.testing.assert_array_equal(src, dst, 'Incorrect extraction of P11')
